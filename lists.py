1cbec65b11e3d7cc63bfd5d476701595fc6b0d5a
james jimenez <asunahaley@gmail.com>
2025-07-22 16:24:53 +0800

    added list

---
+++ lists.py
@@ -0,0 +1,15 @@
+
+
+foods = ["pizza", "burger", "fries", "steak"]
+
+foods[0] = "sushi"
+
+
+foods.append("addedIce")
+foods.remove("sushi")
+foods.insert(0, "pizza")
+foods.sort()
+
+
+for i in foods:
+    print(i)
