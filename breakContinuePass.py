c88164d73001b12def1847bb407b743ccd387430
james jimenez <asunahaley@gmail.com>
2025-07-22 16:03:42 +0800

    break continue pass

---
+++ breakContinuePass.py
@@ -0,0 +1,30 @@
+#loops with break, continue, and pass
+
+#break is nasa while loop.
+
+
+number = "123-456-7890"
+
+#for i in number:
+ #   if i == "-":
+  #      continue
+   # print(i)
+
+#no dash
+
+
+
+#-------------#
+
+
+for i in range(21):
+    if i == 13:
+        pass
+    else:
+        print(i)
+
+
+
+
+
+
