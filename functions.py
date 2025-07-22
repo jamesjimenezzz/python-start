1b67081b33521ef0698927300b819c2176163bd6
james jimenez <asunahaley@gmail.com>
2025-07-22 18:10:08 +0800

    added return functions

---
+++ functions.py
@@ -0,0 +1,28 @@
+
+
+def hello(first, last, age):
+    return print("hello, your first name is" + first + "your last name is " + last + "you are " + str(age) + " years old" );
+
+
+
+first_name = "James"
+last_name = "Jimenez"
+age = 17
+
+
+# hello(first_name, last_name, age)
+
+
+
+
+def multiply (num1, num2):
+    result = num1 * num2
+    return result;
+
+
+
+
+output = multiply(5,6)
+
+print(output)
+
