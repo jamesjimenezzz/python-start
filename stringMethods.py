ec1acb60aafe0ddd631b75c57170cd34636554bf
james jimenez <asunahaley@gmail.com>
2025-07-22 15:29:21 +0800

    intial commit

---
+++ stringMethods.py
@@ -0,0 +1,13 @@
+
+
+name = "James Jimenez "
+
+print(len(name)) #5
+print(name.find("e")) #3
+print(name.capitalize()) #Caplital First Letter
+print(name.upper()) #CAPITAL LETTERS
+print(name.lower()) # lowercase letters
+print(name.isdigit()) # boolean / false
+print(name.count("J")) # = 2
+print(name.replace("J", "a")) #aames aiminez
+print(name *3) #James Jimenez James Jimenez James Jimenez
---
+++ typeCast.py
@@ -0,0 +1,12 @@
+
+
+x = 2 #int
+y = 2.2 #float
+z = "2" #string
+
+
+z = int(z) #conversion. for string = str
+
+print(z*3)
+
+
