86a5223c3908a875ca4b2ea417a41297af0820eb
james jimenez <asunahaley@gmail.com>
2025-07-22 15:30:14 +0800

    moved files

---
+++ main.py
@@ -0,0 +1,11 @@
+import math
+pi = 3.14
+numOfArray = [2, 4, 6]
+
+print(math.ceil(pi)) #round up if it has decimal, except for 0 (whole number)
+print(math.floor(pi)) #round down, stays to the whole number
+print (math.pow(pi, 2)) #power of 2
+print (round(pi)) #roundoff
+print (math.sqrt(420))  #square root of 420
+print(max(numOfArray)) #6
+print(min(numOfArray)) #2
---
+++ var.py
@@ -0,0 +1,12 @@
+
+
+#input / output
+name = input("What is your name? ")
+age = int(input("What is your age?"))
+print(name + " is " + str(age) + " years old") #J ames is 17 years old
+
+
+#multiple assignment
+name, age, isSleepy = "James", 17, True
+print(name, age, isSleepy) #James 17  True
+
