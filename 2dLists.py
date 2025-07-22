b6f99be56bb0d8f68275b4b9adf24d4bb1935071
james jimenez <asunahaley@gmail.com>
2025-07-22 17:09:01 +0800

    added tuples

---
+++ 2dLists.py
@@ -0,0 +1,9 @@
+
+
+drinks = ["water", "coffee", "softdrinkss"]
+dinner = ["pizza", "hamburger", "hotdog" ]
+dessert = ["cake", "ice cream"]
+
+food = [drinks, dinner, dessert]
+
+print(food[0][1])
---
+++ tuples.py
@@ -0,0 +1,12 @@
+
+
+student = ("Bro", "Bro", 21, "Male")
+
+print(student.index("Male"))
+
+for x in student:
+    print(x)
+
+
+if "Bro" in student:
+    print("Hey there bro!")
