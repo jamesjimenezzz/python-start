9d335c92dbc0f3c83583f8af4d40bdee800580be
james jimenez <asunahaley@gmail.com>
2025-07-23 00:16:23 +0800

    added 2dlist

--- 2dLists.py
+++ 2dLists.py
@@ -1,9 +1,31 @@
 
+fruits = ["apple", "orange", "banana", "coconut"]
+vegetables = ["celery", "carrots", "potatoes"]
+meats = ["chicken", "fish", "turkey"]
 
-drinks = ["water", "coffee", "softdrinkss"]
-dinner = ["pizza", "hamburger", "hotdog" ]
-dessert = ["cake", "ice cream"]
 
-food = [drinks, dinner, dessert]
+groceries = [fruits, vegetables, meats]
 
-print(food[0][1])
+
+for grocery in groceries:
+    for food in grocery:
+        print(food, end=" ")
+    
+    print() 
+
+
+
+#--NUMPAD-----------
+
+
+col1 = (1, 2, 3)
+col2= (4,5,6)
+col3= (7,8,9)
+col4 = ("*", 0, "#")
+
+all_columns = (col1, col2, col3, col4)
+
+for i in all_columns:
+    print()
+    for j in i:
+        print(j, end=" ")
