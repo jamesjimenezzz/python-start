a5067889375e69595f33d2e6dd95a6b6e3920504
james jimenez <asunahaley@gmail.com>
2025-07-22 15:38:31 +0800

    added if statement with logical operators

---
+++ ifStatements.py
@@ -0,0 +1,10 @@
+#0-30 temp is good
+#less than 0, and over 30, temp is bad
+
+temp = int(input("What is the temperature today? "))
+
+
+if temp >= 0 and temp <=30:
+    print("The temp is good today")
+else:
+    print("The temp is bad")
