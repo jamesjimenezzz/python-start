ff7d7a0e77128e1dbab9754d797b16fd9abeb072
james jimenez <asunahaley@gmail.com>
2025-07-22 15:43:58 +0800

    added loops

---
+++ forLoop.py
@@ -0,0 +1,21 @@
+
+
+#for i in range(10):
+ #   print(i + 1) #1 2 3 4 - 10
+
+
+rows = int(input("Enter rows: "))
+columns = int(input("Enter columns: "))
+symbol = input("Enter symbol: ")
+
+
+for i in range(rows):
+    for j in range(columns):
+        print(symbol, end="")
+
+    print()
+
+
+#@@@@
+#@@@@
+#@@@@
---
+++ whileLoop.py
@@ -0,0 +1,8 @@
+
+
+while True:
+    name = input("Enter your name: ")
+    if name != "":
+        break
+
+print ("Hello " + name)
