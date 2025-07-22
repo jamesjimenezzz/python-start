cfb8c94313c98732475fa5df68305eebb2a31ee7
james jimenez <asunahaley@gmail.com>
2025-07-22 15:35:31 +0800

    added string slice

---
+++ stringSlicing.py
@@ -0,0 +1,13 @@
+name = "James Jimenez"
+#first_name = name[::2] #JmsJmnz
+#first_name = name[:5] #James
+#first_name = name[::-1] #zenemiJ semaJ
+
+
+website = "https://google.com"
+
+slice = slice(8, -4 )
+
+
+#print(first_name)
+print(website[slice]) # google
