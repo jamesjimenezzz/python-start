8fb637cb9c3b8b54c4d02e9d58921a3f823d3541
james jimenez <asunahaley@gmail.com>
2025-07-25 14:27:05 +0800

    added class variables

--- oop/start.py
+++ oop/start.py
@@ -1,14 +1,8 @@
-
-
-class Car:
-    def __init__(self, model, year, color, for_sale):
-        self.model =  model
-        self.year = year
-        self.color = color
-        self.for_sale = for_sale
+from car import Car
 
 
 
 car1 = Car("Mustang", "2025", "Red", "False")
 
-print(car1.model)
+
+car1.drive()
