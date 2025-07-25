950042ad89c1bd43f7eae7ebd70d3b1a7446c596
james jimenez <asunahaley@gmail.com>
2025-07-25 16:09:09 +0800

    added another one for clarity

--- oop/multipleInheritance.py
+++ oop/multipleInheritance.py
@@ -16,10 +16,19 @@ class Dog(Animal):
         print("Woof")
 
 
+class Cat(Animal):
+    def bark(self):
+        print("Meow")
+
+
 
 dog1 = Dog("Corgi")
-
+cat1 = Cat("Persian")
 
 print (dog1.name)
 dog1.eat()
 dog1.bark()
+
+print(cat1.name)
+cat1.eat()
+cat1.bark()
